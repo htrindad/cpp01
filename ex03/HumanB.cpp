a36f3e7c59913f63924665b06be9f2edaac06140
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-09-01 16:31:28 +0100

    new pointer

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,24 +6,25 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 14:35:26 by htrindad          #+#    #+#             */
-/*   Updated: 2025/09/01 16:13:13 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/09/01 16:17:30 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.h"
 
 //CnD
-HumanB::HumanB() {};
+HumanB::HumanB() : _weapon(NULL) {};
 HumanB::~HumanB() {};
-HumanB::HumanB(std::string name) { setName(name); }
+HumanB::HumanB(std::string name) : _weapon(NULL) { setName(name); }
 
 //Methods
 void HumanB::attack()
 {
-	std::cout << _name << " attacks with their " << getWeapon()->getType() << '\n';
+	if (getWeapon() != NULL)
+		std::cout << _name << " attacks with their " << getWeapon()->getType() << '\n';
 }
 
 void HumanB::setName(std::string name) { _name = name; }
-void HumanB::setWeapon(Weapon weapon) { *_weapon = weapon; }
+void HumanB::setWeapon(Weapon weapon) { _weapon = &weapon; }
 const Weapon *HumanB::getWeapon() { return _weapon; }
 const std::string HumanB::getName() { return _name; }
